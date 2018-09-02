8fa117b0fe628ede7a4ce8346b0c0c5a007101f8
anshika-20 <42172944+anshika-20@users.noreply.github.com>
2018-09-02 23:05:51 +0530

    Add files via upload

---
+++ lab5_q23.cpp
@@ -0,0 +1,17 @@
+//write a C++ program to print all natural number in reverse (from n to 1)-using while loop
+//include library
+#include <iostream>
+using namespace std;
+//including function
+int main() {
+//declaring variables
+cout << "enter the number";
+cin >> a;
+//stating conditions
+for (int a = n;a >= 1; a --){
+sum += a;
+}
+cout << "sum =" << sum;
+//ending
+return 0;
+}
